84ef501e913edf783bc99b72549791892b955448
Tomek <tomsolga90@gmail.com>
2022-12-28 20:21:00 +0100

    db connection estamblish

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,9 +1,18 @@
 import { Module } from "@nestjs/common";
 import { Connection } from "typeorm";
-import { TypeOrmModule } from "@nestjs/typeorm"
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-    imports: [TypeOrmModule.forRoot()],
+    imports: [TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: process.env.DB_PASSWORD,
+        database: 'rentars',
+        entities: [],
+        synchronize: true
+      })],
     exports: [TypeOrmModule]
 })
 export class DatabaseModule {
