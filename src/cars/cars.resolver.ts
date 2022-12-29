e301609a3b7b3c3ba83ef242b6620f011668cd3f
Tomek <tomsolga90@gmail.com>
2022-12-29 20:18:57 +0100

    add cars resolver

---
+++ src/cars/cars.resolver.ts
@@ -0,0 +1,9 @@
+import { Resolver } from "@nestjs/graphql";
+import { CarsService } from "./cars.service";
+
+@Resolver()
+export class CarsResolver {
+    constructor(private carsService: CarsService) {
+        
+    }
+}
