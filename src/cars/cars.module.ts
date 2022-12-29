a7830079917f059d5a67a0d22f26a6a068065620
Tomek <tomsolga90@gmail.com>
2022-12-29 20:16:16 +0100

    add cars module and service

---
+++ src/cars/cars.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { CarsService } from "./cars.service";
+
+
+@Module({
+    imports: [],
+    providers: [CarsService],
+    exports: [CarsService],
+})
+export class CarsModule {
+
+}
