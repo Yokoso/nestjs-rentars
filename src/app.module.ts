0713000708f75d33134e4f5c85ac67637de1dc7a
Tomek <tomsolga90@gmail.com>
2022-12-30 17:28:29 +0100

    add components module as modules bridge

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,20 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { GraphQLModule } from "@nestjs/graphql";
 import { ApolloDriver } from "@nestjs/apollo";
+import { ComponentsModule } from "./components/components.module";
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, GraphQLModule.forRoot({
-    driver: ApolloDriver,
-    playground: (process.env.NODE_ENV === "development" && true) || false,
-    debug: (process.env.NODE_ENV === "development" && true) || false,
-    autoSchemaFile: true,
-  }) ],
+  imports: [
+    ConfigModule.forRoot(), 
+    DatabaseModule, 
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      playground: (process.env.NODE_ENV === "development" && true) || false,
+      debug: (process.env.NODE_ENV === "development" && true) || false,
+      autoSchemaFile: true,
+    }) ,
+    ComponentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
