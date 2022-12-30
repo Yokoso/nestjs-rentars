719e3d9a05720c508fcac33f8b9531d1675ad879
Tomek <tomsolga90@gmail.com>
2022-12-30 18:26:08 +0100

    car entity add and connect to services

--- src/components/cars/cars.resolver.ts
+++ src/components/cars/cars.resolver.ts
@@ -1,14 +1,15 @@
 import { Resolver, Query } from "@nestjs/graphql";
 import { CarsService } from "./cars.service";
+import { Car } from "./entities/car";
 
 @Resolver()
 export class CarsResolver {
-    constructor(private carsService: CarsService) {
-        
-    }
+    constructor(private carsService: CarsService) {}
 
-    @Query(returns => String)
-    public async cars() {
-        return "hello rentars";
+    @Query(returns => [Car])
+    public async cars() : Promise<Car[]> {
+        return await this.carsService.getAllCars().catch(err => {
+            throw err;
+        });
     }
 }
