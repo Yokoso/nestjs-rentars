719914d32c686754bb7bc725a713fdff0af7ea6c
Tomek <tomsolga90@gmail.com>
2023-01-03 22:42:35 +0100

    add validation pkg and basics

--- src/components/cars/dto/new-car.input.ts
+++ src/components/cars/dto/new-car.input.ts
@@ -1,4 +1,5 @@
 import { Field, InputType, Int } from "@nestjs/graphql";
+import { Max, Min } from "class-validator";
 
 
 @InputType()
@@ -8,9 +9,13 @@ export class NewCarInput {
     name: string;
 
     @Field(type => Int)
+    @Min(10, { message: "Daily price can't be lower than 10" })
+    @Max(1000, { message: "Monthly price can't be greater than 1000" })
     dailyPrice: number;
 
     @Field(type => Int)
+    @Min(300, { message: "Monthly price can't be lower than 300" })
+    @Max(30000, { message: "Monthly price can't be greater than 30000" })
     monthlyPrice: number;
 
     @Field()
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import "reflect-metadata";
 import { BaseExceptionFilter, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common/pipes";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new BaseExceptionFilter());
+  // app.useGlobalFilters(new BaseExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT || 9000);
 }
